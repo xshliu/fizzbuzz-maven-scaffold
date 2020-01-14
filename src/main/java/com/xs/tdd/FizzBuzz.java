d9133d6d3122d706c7d6ca2847b9fe432c34ca1f
xsliu <liuxusheng@unionpay.com>
2020-01-14 22:47:51 +0800

    if后语句使用大括号包含

--- src/main/java/com/xs/tdd/FizzBuzz.java
+++ src/main/java/com/xs/tdd/FizzBuzz.java
@@ -3,8 +3,12 @@ package com.xs.tdd;
 public class FizzBuzz {
     public static String of(int in) {
         String s = Integer.toString(in);
-        if (in % 3 == 0) s = "Fizz";
-        if (in % 5 == 0) s = "Fizz".equals(s) ? "FizzBuzz" : "Buzz";
+        if (in % 3 == 0) {
+            s = "Fizz";
+        }
+        if (in % 5 == 0) {
+            s = "Fizz".equals(s) ? "FizzBuzz" : "Buzz";
+        }
         return s;
     }
 }
